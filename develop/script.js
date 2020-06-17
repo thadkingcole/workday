54ec3291244e7c4017cde2ee68456983698c0ad8
Thad Cole <thadjcole@gmail.com>
2020-06-17 14:20:08 -0400

    add rest of time rows that dynamically format based on current time

--- develop/script.js
+++ develop/script.js
@@ -1,3 +1,23 @@
+/*global moment*/
+// Globals
+const today = moment().format("dddd, MMMM Do YYYY"), // today's day month date year
+  startHour = moment().hour(parseInt($(".hour").first().text())), // start workday
+  now = moment(), // current time #am/pm
+  endHour = moment().hour(parseInt($(".hour").last().text())); // end workday
+
+// main
 // put current day at top of page below jumbotron
-const today = moment().format("dddd, MMMM Do YYYY"); // string day month date year
 $("#currentDay").text(today);
+
+// format textareas compared to current time
+$("textarea").map(function () {
+  const areaHour = moment().hour(parseInt($(this).attr("name")));
+  console.log(areaHour);
+  if (areaHour.isBefore(now, "h")) {
+    $(this).addClass("past");
+  } else if (areaHour.isAfter(now, "h")) {
+    $(this).addClass("future");
+  } else {
+    $(this).addClass("present");
+  }
+});
