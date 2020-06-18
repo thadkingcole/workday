2b31082b9c7f42f49a5187db3fed70da835cd90b
Thad Cole <thadjcole@gmail.com>
2020-06-18 11:17:28 -0400

    save buttons for each hour save respective textarea to localstorage. localstorage is loaded & displayed upon page load.

--- develop/script.js
+++ develop/script.js
@@ -1,18 +1,18 @@
 /*global moment*/
-// Globals
-const today = moment().format("dddd, MMMM Do YYYY"), // today's day month date year
-  startHour = moment().hour(parseInt($(".hour").first().text())), // start workday
-  now = moment(), // current time #am/pm
-  endHour = moment().hour(parseInt($(".hour").last().text())); // end workday
 
-// main
-// put current day at top of page below jumbotron
-$("#currentDay").text(today);
+const today = moment().format("dddd, MMMM Do YYYY"); // today's day month date year
+$("#currentDay").text(today); // display current day in jumbotron
 
-// format textareas compared to current time
+/*
+  for each textarea, apply format based on current time
+  & load events from local storage
+*/
 $("textarea").map(function () {
-  const areaHour = moment().hour(parseInt($(this).attr("name")));
-  console.log(areaHour);
+  const areaHour = moment().hour(parseInt($(this).attr("name"), 10)), // moment.js object for respective textarea
+    event = localStorage.getItem(areaHour.hour()), // saved event
+    now = moment(); // current time #am/pm
+
+  // format textarea based on local time
   if (areaHour.isBefore(now, "h")) {
     $(this).addClass("past");
   } else if (areaHour.isAfter(now, "h")) {
@@ -20,4 +20,17 @@ $("textarea").map(function () {
   } else {
     $(this).addClass("present");
   }
+
+  // display saved event from local storage if there is one for that hour
+  if (event) {
+    $(this).val(event);
+  }
+});
+
+// event listener for all save buttons
+$(".saveBtn").click(function () {
+  const event = $(this).prev().val(), // text in textarea being saved
+    time = $(this).prev().attr("name"); // respective textarea hour
+  // save event for hour
+  localStorage.setItem(time, event);
 });
